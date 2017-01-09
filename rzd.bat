7c2b9dd12abe87faf1106d79e515223aaeea5290
sergvcx <sergvcx@mail.ru>
2017-01-09 23:24:46 +0300

    commit from nout

--- rzd.bat
+++ rzd.bat
@@ -1,9 +1,10 @@
 for /L %%x in (1, 1, 1000) do (
-   @echo ============ 05.01.2017 ==========
-   del *.png && casperjs rzd-old.js --auth=auth.json --ticket=msk-sarov.json --bank=hkb.json --pass=serg.json --date=05.01.2017
+   @echo ============ 08.01.2017 ==========
+   del *.png && casperjs rzd-old.js --auth=auth.json --ticket=sarov-msk.json --bank=hkb.json --pass=serg.json --date=10.01.2017
    @echo ----------------------------------
    sleep 30
 )
+exit
 
    @echo ============ 08.01.2017 ==========
    del *.png && casperjs rzd-old.js --auth=auth.json --ticket=sarov-msk.json --bank=hkb.json --pass=serg.json --date=08.01.2017
